179a25dee0aeab67bb9860620be75eb023b6f6ee
yad <yad@yad.com>
2026-08-01 20:25:34 +0200

    feat: add spell

--- modules/mod-flightmaster-whistle/data/sql/db-world/2026_08_01_00_flightmaster_whistle_spell.sql
+++ modules/mod-flightmaster-whistle/data/sql/db-world/2026_08_01_00_flightmaster_whistle_spell.sql
@@ -1,77 +1,172 @@
+-- ITEM TEMPLATE
 DELETE FROM `item_template` WHERE `entry` = 70000;
-INSERT INTO `item_template` (`entry`,`class`,`subclass`,`SoundOverrideSubclass`,`name`,`displayid`,`Quality`,`Flags`,`FlagsExtra`,`BuyCount`,`BuyPrice`,`SellPrice`,`InventoryType`,`AllowableClass`,`AllowableRace`,`ItemLevel`,`RequiredLevel`,`RequiredSkill`,`RequiredSkillRank`,`requiredspell`,`requiredhonorrank`,`RequiredCityRank`,`RequiredReputationFaction`,`RequiredReputationRank`,`maxcount`,`stackable`,`ContainerSlots`,`stat_type1`,`stat_value1`,`stat_type2`,`stat_value2`,`stat_type3`,`stat_value3`,`stat_type4`,`stat_value4`,`stat_type5`,`stat_value5`,`stat_type6`,`stat_value6`,`stat_type7`,`stat_value7`,`stat_type8`,`stat_value8`,`stat_type9`,`stat_value9`,`stat_type10`,`stat_value10`,`ScalingStatDistribution`,`ScalingStatValue`,`dmg_min1`,`dmg_max1`,`dmg_type1`,`dmg_min2`,`dmg_max2`,`dmg_type2`,`armor`,`holy_res`,`fire_res`,`nature_res`,`frost_res`,`shadow_res`,`arcane_res`,`delay`,`ammo_type`,`RangedModRange`,`spellid_1`,`spelltrigger_1`,`spellcharges_1`,`spellppmRate_1`,`spellcooldown_1`,`spellcategory_1`,`spellcategorycooldown_1`,`spellid_2`,`spelltrigger_2`,`spellcharges_2`,`spellppmRate_2`,`spellcooldown_2`,`spellcategory_2`,`spellcategorycooldown_2`,`spellid_3`,`spelltrigger_3`,`spellcharges_3`,`spellppmRate_3`,`spellcooldown_3`,`spellcategory_3`,`spellcategorycooldown_3`,`spellid_4`,`spelltrigger_4`,`spellcharges_4`,`spellppmRate_4`,`spellcooldown_4`,`spellcategory_4`,`spellcategorycooldown_4`,`spellid_5`,`spelltrigger_5`,`spellcharges_5`,`spellppmRate_5`,`spellcooldown_5`,`spellcategory_5`,`spellcategorycooldown_5`,`bonding`,`description`,`PageText`,`LanguageID`,`PageMaterial`,`startquest`,`lockid`,`Material`,`sheath`,`RandomProperty`,`RandomSuffix`,`block`,`itemset`,`MaxDurability`,`area`,`Map`,`BagFamily`,`TotemCategory`,`socketColor_1`,`socketContent_1`,`socketColor_2`,`socketContent_2`,`socketColor_3`,`socketContent_3`,`socketBonus`,`GemProperties`,`RequiredDisenchantSkill`,`ArmorDamageModifier`,`duration`,`ItemLimitCategory`,`HolidayId`,`ScriptName`,`DisenchantID`,`FoodType`,`minMoneyLoot`,`maxMoneyLoot`,`flagsCustom`,`VerifiedBuild`) VALUES (70000,15,0,-1,'Flightmaster Whistle',58859,4,64,0,1,0,0,0,-1,-1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,110001,0,0,0,-1,0,-1,0,0,0,0,-1,0,-1,0,0,0,0,-1,0,-1,0,0,0,0,-1,0,-1,0,0,0,0,-1,0,-1,1,'Teleports you to the nearest flight master in your area.',0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,'flightmaster_whistle',0,0,0,0,0,-1);
 
-DELETE FROM `item_dbc` WHERE `ID` = 70000;
-INSERT INTO `item_dbc` (`ID`,`ClassID`,`SubclassID`,`Sound_Override_Subclassid`,`Material`,`DisplayInfoID`,`InventoryType`,`SheatheType`) VALUES (70000,15,0,-1,-1,58859,0,0);
-
-DELETE FROM `spell_script_names` WHERE `spell_id` = 110001;
-
-INSERT INTO `spell_script_names`
-(`spell_id`, `ScriptName`)
+INSERT INTO `item_template`
+(
+    `entry`,
+    `class`,
+    `subclass`,
+    `SoundOverrideSubclass`,
+    `name`,
+    `displayid`,
+    `Quality`,
+    `Flags`,
+    `BuyCount`,
+    `BuyPrice`,
+    `SellPrice`,
+    `InventoryType`,
+    `AllowableClass`,
+    `AllowableRace`,
+    `ItemLevel`,
+    `RequiredLevel`,
+    `maxcount`,
+    `stackable`,
+    `spellid_1`,
+    `spelltrigger_1`,
+    `spellcharges_1`,
+    `spellcooldown_1`,
+    `bonding`,
+    `description`,
+    `ScriptName`
+)
 VALUES
-(110001, 'spell_flightmaster_whistle_cast');
+(
+    70000,
+    15,
+    0,
+    -1,
+    'Flightmaster Whistle',
+    15798,
+    3,
+    64,
+    1,
+    0,
+    0,
+    0,
+    -1,
+    -1,
+    1,
+    0,
+    1,
+    1,
+    110001,
+    0,
+    0,
+    -1,
+    1,
+    'Teleports you to the nearest flightmaster.',
+    'flightmaster_whistle'
+);
 
 
-DELETE FROM `spell_dbc` WHERE `ID` = 110001;
 
-INSERT INTO `spell_dbc`
+-- ITEM DBC
+DELETE FROM `item_dbc`
+WHERE `ID` = 70000;
+
+INSERT INTO `item_dbc`
 (
     `ID`,
-    `CastingTimeIndex`,
-    `RecoveryTime`,
-    `DurationIndex`,
-    `RangeIndex`,
-    `Effect_1`,
-    `ImplicitTargetA_1`,
-    `Targets`,
-    `Name_Lang_enUS`,
-    `Name_Lang_frFR`,
-    `Description_Lang_enUS`,
-    `Description_Lang_frFR`,
-    `EquippedItemClass`
+    `ClassID`,
+    `SubclassID`,
+    `Sound_Override_Subclassid`,
+    `Material`,
+    `DisplayInfoID`,
+    `InventoryType`,
+    `SheatheType`
+)
+VALUES
+(
+    70000,
+    15,
+    0,
+    -1,
+    -1,
+    15798,
+    0,
+    0
+);
+
+
+
+-- SPELL SCRIPT
+DELETE FROM `spell_script_names`
+WHERE `spell_id` = 110001;
+
+INSERT INTO `spell_script_names`
+(
+    `spell_id`,
+    `ScriptName`
 )
 VALUES
 (
     110001,
+    'spell_flightmaster_whistle_cast'
+);
+
+
+
+-- SPELL DBC
+DELETE FROM `spell_dbc`
+WHERE `ID` = 110001;
+
+
+INSERT INTO `spell_dbc`
+(
+    `ID`,
+    `Targets`,
+    `CastingTimeIndex`,
+    `RecoveryTime`,
+    `DurationIndex`,
+    `RangeIndex`,
+    `EquippedItemClass`,
+    `Effect_1`,
+    `ImplicitTargetA_1`,
+    `ImplicitTargetB_1`,
+    `EffectMiscValue_1`,
+    `Name_Lang_enUS`,
+    `Name_Lang_frFR`,
+    `Description_Lang_enUS`,
+    `Description_Lang_frFR`
+)
+VALUES
+(
+    110001,
+    1,
     7,
     900000,
     0,
     1,
+    -1,
     77,
     1,
-    1,
+    0,
+    0,
     'Flightmaster Whistle',
     'Sifflet de maître de vol',
     'Teleports you to the nearest flightmaster.',
-    'Vous téléporte auprès du maître de vol le plus proche.',
-    -1
+    'Vous téléporte auprès du maître de vol le plus proche.'
 );
 
-UPDATE `item_template`
-SET
-    `displayid` = 15798,
-    `Quality` = 3,
-    `description` = 'Teleports you to the nearest flightmaster.',
-    `ScriptName` = 'flightmaster_whistle'
-WHERE `entry` = 70000;
 
+
+-- LOCALE ITEM
 DELETE FROM `item_template_locale`
 WHERE `ID` = 70000
 AND `locale` = 'frFR';
+
 
 INSERT INTO `item_template_locale`
 (
     `ID`,
     `locale`,
     `Name`,
-    `Description`,
-    `VerifiedBuild`
+    `Description`
 )
 VALUES
 (
     70000,
     'frFR',
     'Sifflet de maître de vol',
-    'Vous demandez un transfert jusqu’au maître de vol le plus proche.',
-    NULL
+    'Vous demandez un transfert jusqu\'au maître de vol le plus proche.'
 );
--- modules/mod-flightmaster-whistle/src/mod_flightmaster_whistle_itemscript.cpp
+++ modules/mod-flightmaster-whistle/src/mod_flightmaster_whistle_itemscript.cpp
@@ -1,51 +1,72 @@
-/*
- * Credits: silviu20092
- */
-
-#include "Player.h"
 #include "ScriptMgr.h"
 #include "SpellScript.h"
 #include "SpellScriptLoader.h"
+#include "Player.h"
+#include "Item.h"
 #include "flightmaster_whistle.h"
+
 
 enum FlightmasterWhistleSpells
 {
     SPELL_FLIGHTMASTER_WHISTLE_CAST = 110001
 };
 
+
 class spell_flightmaster_whistle_cast : public SpellScript
 {
     PrepareSpellScript(spell_flightmaster_whistle_cast);
 
-    void HandleScript(SpellEffIndex /*effIndex*/)
+    void HandleScriptEffect(SpellEffIndex)
     {
-        if (Player* player = GetHitPlayer())
-            sFlightmasterWhistle->TeleportToNearestFlightmaster(player);
+        Player* player = GetCaster()->ToPlayer();
+
+        if (!player)
+            return;
+
+        sFlightmasterWhistle->TeleportToNearestFlightmaster(player);
     }
+
 
     void Register() override
     {
-        OnEffectHitTarget += SpellEffectFn(spell_flightmaster_whistle_cast::HandleScript, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+        OnEffectHit += SpellEffectFn(
+            spell_flightmaster_whistle_cast::HandleScriptEffect,
+            EFFECT_0,
+            SPELL_EFFECT_SCRIPT_EFFECT
+        );
     }
 };
 
-class flightmaster_whistle : public ItemScript
+
+
+class item_flightmaster_whistle : public ItemScript
 {
 public:
-    flightmaster_whistle() : ItemScript("flightmaster_whistle") {}
 
-    bool OnUse(Player* player, Item* /*item*/, SpellCastTargets const& /*targets*/) override
+    item_flightmaster_whistle()
+        : ItemScript("flightmaster_whistle")
     {
-        if (player)
-        {
-            player->CastSpell(player, SPELL_FLIGHTMASTER_WHISTLE_CAST, false);
-        }
+    }
+
+
+    bool OnUse(Player* player, Item* item, SpellCastTargets const& targets) override
+    {
+        player->CastSpell(
+            player,
+            SPELL_FLIGHTMASTER_WHISTLE_CAST,
+            false,
+            item
+        );
+
         return true;
     }
 };
 
+
+
 void AddSC_mod_flightmaster_whistle_itemscript()
 {
     RegisterSpellScript(spell_flightmaster_whistle_cast);
-    new flightmaster_whistle();
+
+    new item_flightmaster_whistle();
 }
